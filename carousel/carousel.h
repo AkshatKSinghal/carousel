b05b9fdf1c9e0b7a42413dc38dc7742d3c5ba061
Akshat Singhal <akshat.kumar.singhal@gmail.com>
2017-09-23 12:04:37 +0530

    Initial Commit

---
+++ carousel/carousel.h
@@ -0,0 +1,19 @@
+//
+//  carousel.h
+//  carousel
+//
+//  Created by Akshat Singhal on 23/09/17.
+//  Copyright © 2017 Akshat Singhal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for carousel.
+FOUNDATION_EXPORT double carouselVersionNumber;
+
+//! Project version string for carousel.
+FOUNDATION_EXPORT const unsigned char carouselVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <carousel/PublicHeader.h>
+
+
